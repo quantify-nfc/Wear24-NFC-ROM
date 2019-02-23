c7916648700ec297fc3b40a208ac68776ad656ca
Jared Tamana <seanythedream@gmail.com>
2019-02-22 19:11:50 -0500

    Fix system build script

--- build-system.sh
+++ build-system.sh
@@ -2,13 +2,25 @@
 export DATE_TIME=$(date +"%m-%d-%Y_%H-%M-%S")
 export branch=$(git symbolic-ref --short HEAD)
 
-yes | cp -af system-new/* unpack/ # copy altered files to unpack dir
+# in the future, uncomment these lines to back up stock files that are modified by us
+# ---------------------------------------------------------------
+# mkdir oldfiles
+# echo "Backing up unmodified files to oldfiles"
+# yes | cp -af system-deodexed-stock/{[INSERT FILES HERE]} oldfiles # back up stock files that are being modified
 
-mkdir middleman
+echo "Copying new/modified files to stock directory..."
+yes | cp -af system-new/* system-deodexed-stock/ # copy altered files to stock dir
 
-make_ext4fs -s -l 786432000 -a system system.img.new middleman #SHOULD be a sparse image!
+mkdir out
+echo "Making sparse image to out using make_ext4fs"
+make_ext4fs -s -l 786432000 -a system system.img.new out #SHOULD be a sparse image!
 
+echo "Converting sparse image to .new.dat"
 ./tools/img2sdat out/system.img.new -o aromainstaller -p system.new.dat #output system.new.dat to aroma zip for building
+rm -f out/system.img.new
 
-zip -j JareDav-$DATE_TIME-$branch aromainstaller/*
+cd aromainstaller
+echo "Zipping AROMA..."
+zip -r ../Quantify-$DATE_TIME-$branch.zip *
+cd ..
 echo "ROM ZIP can be found at $(pwd)"
