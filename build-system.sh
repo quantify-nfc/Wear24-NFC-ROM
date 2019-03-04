012a645f58a8e4b9f2564181d0d270baa2ed2de3
Jared Tamana <seanythedream@gmail.com>
2019-03-03 23:30:36 -0500

    Partially fix sparse image build... please see #25

--- build-system.sh
+++ build-system.sh
@@ -11,9 +11,11 @@ export branch=$(git symbolic-ref --short HEAD)
 echo "Copying new/modified files to stock directory..."
 yes | cp -af system-new/* system-deodexed-stock/ # copy altered files to stock dir
 
+export systemdirsize=$(du -sb system-deodexed-stock | cut -f1)
+
 mkdir out
 echo "Making sparse image to out using make_ext4fs"
-make_ext4fs -s -l 786432000 -a system out/system.img.new #SHOULD be a sparse image!
+make_ext4fs -S file_contexts -l $systemdirsize -a system system.img.new system-deodexed-stock #SHOULD be a sparse image!
 
 echo "Converting sparse image to .new.dat"
 ./tools/img2sdat/img2sdat.py out/system.img.new -o "zip" #output system.new.dat to zip for building
